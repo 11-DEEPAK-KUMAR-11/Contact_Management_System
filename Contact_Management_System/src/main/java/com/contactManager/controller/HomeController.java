31ca32770309b268defeea8a4ae11fde180c95a2
11-DEEPAK-KUMAR-11 <3511.deepak@gmail.com>
2023-03-17 13:27:49 +0530

    Some html,css setup

---
+++ Contact_Management_System/src/main/java/com/contactManager/controller/HomeController.java
@@ -0,0 +1,24 @@
+package com.contactManager.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/home")
+	public String home(Model model)
+	{
+		model.addAttribute("title", "Home - Contact Manager");
+		return "home";
+	}
+	
+	@GetMapping("/about")
+	public String about(Model model)
+	{
+		model.addAttribute("title", "About - Contact Manager");
+		return "about";
+	}
+}
---
+++ Contact_Management_System/src/main/resources/static/js/script.js
@@ -0,0 +1,1 @@
+console.log("This is contact management system")
