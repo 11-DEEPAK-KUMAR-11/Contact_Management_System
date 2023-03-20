bf261ddc6379cf072d27f119d6edc69dcd5a5b51
11-DEEPAK-KUMAR-11 <3511.deepak@gmail.com>
2023-03-20 17:46:24 +0530

    After login user specific side bar completed

--- Contact_Management_System/src/main/java/com/contactManager/controller/UserController.java
+++ Contact_Management_System/src/main/java/com/contactManager/controller/UserController.java
@@ -1,16 +1,35 @@
 package com.contactManager.controller;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.contactManager.models.User;
+import com.contactManager.repository.UserRepo;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
+	@Autowired
+	private UserRepo userRepo;
+	
 	@GetMapping("/index")
-	public String dashBoard()
+	public String dashBoard(Model model, Principal principal)
 	{
+		//Get the user from username(Email)
+		String userName=principal.getName();
+		System.out.println("User name "+userName);
+		
+		User user=userRepo.findByEmail(userName);
+		System.out.println("User details "+user);
+		
+		model.addAttribute("user", user);
+		
 		return "User/userDashboard";
 	}
 }
--- Contact_Management_System/src/main/resources/static/js/script.js
+++ Contact_Management_System/src/main/resources/static/js/script.js
@@ -1,1 +1,17 @@
-console.log("This is contact management system")
+console.log("This is contact management system");
+
+const toggleSidebar = () => {
+if($(".sidebar").is(":visible"))
+{
+    //if true -> close the tab
+    $(".sidebar").css("display","none");
+    $(".content").css("margin-left","0%");
+
+} else{
+    //if flase -> open the tab (show the tab)
+
+    $(".sidebar").css("display","block");
+    $(".content").css("margin-left","20%");
+}
+
+};
